2b7d34516d86d324ea8d37f3c839b171474bd046
Indra-Haha <124240180@student.upnyk.ac.id>
2026-02-25 17:13:03 +0700

    feat: add model user and product

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'no_hp'
     ];
 
     /**
@@ -31,6 +32,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'role'
     ];
 
     /**
@@ -41,8 +43,7 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
+            'password' => 'bcrypt',
         ];
     }
 }

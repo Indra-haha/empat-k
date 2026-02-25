3bc3e4c4a74db017a7557d58f3bd5655276b42a3
Indra-Haha <124240180@student.upnyk.ac.id>
2026-02-25 17:13:49 +0700

    feat: add schema user & product tables

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,24 +12,19 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->id('user_id')->autoIncrement()->primary();
+            $table->string('username')->unique();
             $table->string('password');
+            $table->string('name');
+            $table->string('no_hp');
+            $table->enum('role', ['pelanggan', 'cs', 'desainer', 'kp','accounting'])->default('pelanggan');
             $table->rememberToken();
             $table->timestamps();
         });
 
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
-
         Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
+            $table->id('id')->primary();
+            $table->foreignId('user_id')->unique()->constrained('users', 'user_id')->cascadeOnDelete();
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
             $table->longText('payload');
@@ -43,7 +38,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
         Schema::dropIfExists('sessions');
     }
 };

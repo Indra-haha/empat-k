1093d302e43498f5dcbf01153ae6acb05fa32d14
Indra-haha <124240180@student.upnyk.ac.id>
2026-04-09 03:27:35 +0700

    chore: change UI

--- resources/js/Components/ProductCard.tsx
+++ resources/js/Components/ProductCard.tsx
@@ -1,19 +1,19 @@
 import React, { JSX } from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
-import { ProductWithCategoryProps } from "@/Types/Products";
+import { ProductDetailProps } from "@/Types/Products";
 import { Link } from "@inertiajs/react";
-export const ProductCard = (product: ProductWithCategoryProps): JSX.Element => {
+export const ProductCard = (product: ProductDetailProps): JSX.Element => {
     return (
         <Link
             href={`/product/${product.product_id}`}
-            className="flex flex-col w-full h-[190px] items-center justify-between relative bg-green-200 rounded-[10px] overflow-hidden"
+            className="flex flex-col w-full h-full items-center justify-between relative bg-green-200 rounded-lg overflow-hidden gap-2"
         >
             <LazyLoadImage
                 src={`/storage/${product.url_img}`}
                 alt={product.name}
-                width={150}
-                height={150}
-                className="object-cover"
+                width={200}
+                height={100}
+                className="object-cover aspect-square"
             />
             <main className="w-full py-2 flex flex-col items-center justify-center relative text-green-800">
                 <h1 className="relative flex items-center w-fit font-semibold text-sm tracking-[0] leading-[normal]">

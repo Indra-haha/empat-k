24b5468dfe914839d1c96ac9006a647d1be802d8
Indra-haha <124240180@student.upnyk.ac.id>
2026-04-09 08:28:50 +0700

    fix: change route statis to dinamis route

--- resources/js/Pages/pelanggan/Layout/PageWithHeaderBack.tsx
+++ resources/js/Pages/pelanggan/Layout/PageWithHeaderBack.tsx
@@ -2,18 +2,18 @@ import { ChevronLeft } from "@untitledui/icons";
 import { Link } from '@inertiajs/react';
 import React from "react";
 
-export function PageWithHeaderBack({ title, children }) {
+export function PageWithHeaderBack({ title, children, route }) {
     return (
         <section className="flex flex-col max-w-[425px] h-screen mx-auto bg-gray-100">
             <header className="h-16 flex items-center border-b-2 border-green-200 bg-green-200 rounded-b-4xl py-2">
-                <main className="max-w-[380px] mx-auto flex flex-row gap-3 w-full px-2">
-                    <Link href="/products" className="text-green-800">
+                <main className="max-w-[380px] mx-5 flex flex-row gap-3 w-full px-2">
+                    <Link href={`/${route}`} className="text-green-800">
                         <ChevronLeft className="text-4xl" />
                     </Link>
                     <h1 className="text-xl font-bold text-green-800">{title}</h1>
                 </main>
             </header>
-            <main className="p-5 h-full max-w-[380px] mx-auto">{children}</main>
+            <main className="p-5 h-auto max-w-[400px] mx-5">{children}</main>
         </section>
     );
 }

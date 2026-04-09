8c02fa13fc3bdd2e665e2ad146a80ea9a1862d8c
Indra-haha <124240180@student.upnyk.ac.id>
2026-04-09 08:22:38 +0700

    perf: add attribut and create mapping to render value

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,12 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\OrderStatusHistory;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
-use Inertia\Response;
 use App\Models\Order;
+use App\Models\Product;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
 class OrderController extends Controller
 {
@@ -21,7 +21,7 @@ class OrderController extends Controller
                 ->where('user_id', $user->user_id)
                 ->map(function ($order) {
                     return [
-                        'no' => $order->product->product_id,
+                        'no' => $order->order_id,
                         'name' => $order->product->name,
                         'url' => $order->product->url_img,
                         'quantity' => $order->quantity,
@@ -33,16 +33,22 @@ class OrderController extends Controller
                 })
 
             :
-            $orders = Order::with('product', 'latestStatus')
+            $orders = Order::with('product', 'request', 'latestStatus', 'user')
                 ->get()
                 ->map(function ($order) {
                     return [
-                        'quantity' => $order->quantity,
                         'no' => $order->order_id,
-                        'url_img' => $order->product->url_img,
+                        'name' => $order->product->name,
+                        'user' => $order->user->name,
+                        'request' => ($order->request_id) ? $order->request_id : null,
+                        'phone' => $order->user->no_hp,
+                        'url_img_product' => $order->product->url_img,
+                        'url_img_request' => ($order->request && $order->request->status === 'finished') ? $order->request->upload_img : null,
                         'ordered_by' => Carbon::parse($order->created_at)
                             ->locale('id')
                             ->translatedFormat('d F Y'),
+                        'quantity' => $order->quantity,
+                        'price' => $order->product->price,
                         'total_price' => $order->total_price, // rename
                         'status' => $order->latestStatus->status,
                     ];
@@ -53,11 +59,6 @@ class OrderController extends Controller
 
     }
 
-    // public function create($id): Response
-    // {
-    //     return Inertia::render(`pelanggan/product/${id}`);
-    // }
-
     public function store(Request $request)
     {
         $this->validate($request, [
@@ -67,46 +68,40 @@ class OrderController extends Controller
             'quantity' => 'required',
         ]);
 
-        DB::transaction(function () use ($request) {
-            $price = DB::table('products')
-                ->where('product_id', $request->product_id)
-                ->value('price');
-
-            $order = Order::create([
-                'user_id' => $request->user_id,
-                'request_id' => $request->request_id,
-                'product_id' => $request->product_id,
-                'quantity' => $request->quantity,
-                'price' => $price,
-                'total_price' => $request->quantity * $price
-            ]);
-
-            DB::table('order_status_histories')->insert([
-                'order_id' => $order->order_id,
-                'status' => $order->status, // ambil dari orders
-                'created_by' => auth()->id(),
-                'created_at' => now(),
-                'updated_at' => now(),
-            ]);
-        });
+        $price = Product::findOrFail($request->product_id)->price;
+        $order = Order::create([
+            'user_id' => $request->user_id,
+            'request_id' => $request->request_id,
+            'product_id' => $request->product_id,
+            'quantity' => $request->quantity,
+            'price' => $price,
+            'total_price' => $request->quantity * $price
+        ]);
+        OrderStatusHistory::create([
+            'order_id' => $order->order_id,
+            'status' => $order->status, // ambil dari orders
+            'created_by' => auth()->id(),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
         return redirect()->route('orders.index')->with('success', 'Order created successfully.');
     }
 
     public function show($id)
     {
-        // return Inertia::render('pelanggan/OrderPage/OrderShow', [
-        //     'order' => "Order masuk show dengan id $id"
-        // ]);
-        $this->authorizeAction('view');
-        $order = Order::with('statusHistories')->where('product_id', $id)
-        ->where('user_id', auth()->id())
-        ->firstOrFail()                                                                                              ;
+        $order = Order::where('order_id', $id)
+            ->where('user_id', auth()->id())
+            ->firstOrFail();
         return Inertia::render('pelanggan/OrderPage/OrderShow', [
             'order' => [
-                'no' => $order->product->product_id,
+                'no' => $order->order_id,
                 'name' => $order->product->name,
-                'url' => $order->product->url_img,  
+                'url' => $order->product->url_img,
+                'category' => $order->product->category->name,
+                'request' => $order->request_id ? $order->request->fee : null,
                 'quantity' => $order->quantity,
+                'price' => $order->product->price,
+                'total_price' => $order->total_price,
                 'ordered_by' => Carbon::parse($order->created_at)
                     ->locale('id')
                     ->translatedFormat('d F Y'),

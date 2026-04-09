a3cdfa202a5f317e063fb4063c31117d5299b93d
Indra-haha <124240180@student.upnyk.ac.id>
2026-04-09 08:24:51 +0700

    feat: create render for pelanggan & desainer

--- app/Http/Controllers/RequestsController.php
+++ app/Http/Controllers/RequestsController.php
@@ -11,16 +11,48 @@ class RequestsController extends BaseController
     public function index()
     {
         $this->authorizeAction('viewAny');
-        $requests = CustomRequest::all();
-        return inertia('requests', compact('requests'));
+        $role = Auth::user()->role;
+        $role === 'desainer' ?
+            $customrequests = CustomRequest::with('product')->get() :
+            $customrequests = CustomRequest::with('product')->where('user_id', Auth::user()->user_id)->get();
+        return Inertia("{$role}/RequestPage/RequestList", [
+            'requests' => $customrequests->map(function ($request) {
+                return [
+                    'no' => $request->request_id,
+                    'user' => $request->user->name,
+                    'upload_image' => $request->upload_img,
+                    'img_product' => $request->product->url_img,
+                    'description' =>
+                        [
+                            'teks' => $request->description['teks_font'] ?? null,
+                            'style' => $request->description['gaya_desain'] ?? null,
+                            'color' => $request->description['warna_dominan'] ?? null,
+                            'reference' => $request->description['referensi_virtual'] ?? null,
+                            'focus_spot' => $request->description['titik_fokus_revisi'] ?? null
+                        ],
+                    'status' => $request->status,
+                    'fee' => $request->fee,
+                    'create' => $request->created_at->format('Y-m-d H:i:s'),
+                ];
+            }),
+        ]);
     }
 
     public function store(Request $request)
     {
         $this->authorizeAction('create');
+        
+        $desc = $request->validate([
+            'teks_font' => 'required|string',
+            'gaya_desain' => 'required|string',
+            'warna_dominan' => 'required|string',
+            'referensi_virtual' => 'required|string',
+            'titik_fokus_revisi' => 'required|string',
+        ]);
+
         $data = $request->validate([
             'product_id' => 'required',
-            'description' => 'required|array',
+            'description' => $desc,
         ]);
 
         CustomRequest::create([

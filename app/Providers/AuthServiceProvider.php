2039db71a41d2c92debef83bf4abef073073a147
Indra-haha <124240180@student.upnyk.ac.id>
2026-04-20 14:25:32 +0700

    auth: integrate schema auth

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -8,6 +8,9 @@ class AuthServiceProvider extends ServiceProvider
 {
     protected $policies = [
         \App\Models\Product::class => \App\Policies\ProductPolicy::class,
+        \App\Models\CustomRequest::class => \App\Policies\CustomRequestPolicy::class,
+        \App\Models\Order::class => \App\Policies\OrderPolicy::class,
+        \App\Models\Invoice::class => \App\Policies\InvoicePolicy::class,
     ];
     /**
      * Register services.

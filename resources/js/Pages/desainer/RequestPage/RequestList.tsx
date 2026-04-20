7a2e6c89d125c40a702a739cd8d32eeedecc480a
Indra-haha <124240180@student.upnyk.ac.id>
2026-04-20 08:38:25 +0700

    refactor: clean code

--- resources/js/Pages/desainer/RequestPage/RequestList.tsx
+++ resources/js/Pages/desainer/RequestPage/RequestList.tsx
@@ -1,22 +1,45 @@
 import { AdminItemCard } from "@/Components/AdminItemCard";
 import AdminLayout from "@/Layouts/AdminLayout";
 import { RequestProps } from "@/Types/Request";
-import React from "react";
+import React, { useState } from "react";
+import RequestModals from "./RequestModals";
 
-export default function RequestList({requests} : {requests: RequestProps[]}) {
+export default function RequestList({
+    requests,
+}: {
+    requests: RequestProps[];
+}) {
     console.log("RequestList component rendered", requests);
+    const [selectedRequest, setSelectedRequest] = useState<RequestProps | null>(
+        null,
+    );
+    const [showModal, setShowModal] = useState(false);
+
     return (
         <AdminLayout>
-            {requests.length > 0 ? 
+            {requests.length > 0 ? (
                 requests.map((request) => (
                     <AdminItemCard
-                        key={request.no} url_img={request.img_product} judul={String(request.no)} tgl={request.create} quantity={0} status={request.status} onClick={function (): void {
-                            throw new Error("Function not implemented.");
-                        } }                    />
-                )) :
+                        key={request.no}
+                        url_img={`${request.img_product}`}
+                        judul={String(request.no)}
+                        tgl={request.create}
+                        status={request.status}
+                        onClick={() => {
+                            setSelectedRequest(request);
+                            setShowModal(true);
+                        }}
+                    />
+                ))
+            ) : (
                 <p>No requests found.</p>
-            }
+            )}
+            {selectedRequest && showModal && (
+                <RequestModals
+                    selectedRequest={selectedRequest}
+                    onClose={() => setShowModal(false)}
+                />
+            )}
         </AdminLayout>
     );
 }
-                   

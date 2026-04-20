eddd1f352630e7f537aa7909b0f0bf8a9655f55d
Indra-haha <124240180@student.upnyk.ac.id>
2026-04-20 08:46:30 +0700

    fix: ubah atribut prop & import tag LazyLoad to img

--- resources/js/Components/AdminItemCard.tsx
+++ resources/js/Components/AdminItemCard.tsx
@@ -1,30 +1,37 @@
 import React from "react";
 import { AdminItemCardProps } from "../Types/AdminItemCard";
 import PrimaryButton from "@/Components/PrimaryButton";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 export function AdminItemCard({
     url_img,
     judul,
     tgl,
-    quantity,
+    keterangan,
     status,
     onClick,
 }: AdminItemCardProps) {
     return (
         <section className="flex flex-row w-full justify-around bg-green-100 mb-4 p-2 rounded-xl">
             <header className="w-1/9 p-2">
-                <img
-                    src={url_img}
+                <LazyLoadImage
+                    src={`/storage/${url_img}`}
                     alt={judul}
-                    className="aspect-square object-cover rounded-md"
+                    width={200}
+                    height={100}
+                    className="object-cover aspect-square"
                 />
             </header>
 
             <main className="flex flex-col w-7/9 p-2 justify-start text-green-800">
-                <h1 className="text-[20px] text-extrabold w-full mb-1">{judul}</h1>
-                <span className="text-[14px] text-normal w-full mb-3">{tgl}</span>
+                <h1 className="text-[20px] text-extrabold w-full mb-1">
+                    {judul}
+                </h1>
+                <span className="text-[14px] text-normal w-full mb-3">
+                    {tgl}
+                </span>
                 <span className="text-[14px] text-normal w-full">
-                    Jumlah : {quantity}
+                    {keterangan}
                 </span>
             </main>
 

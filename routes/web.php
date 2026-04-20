4c0383efdcec26be2288a4ac5cf55c2888b01ba6
Indra-haha <124240180@student.upnyk.ac.id>
2026-04-20 08:34:15 +0700

    feat: add route edit in request

--- routes/web.php
+++ routes/web.php
@@ -41,6 +41,11 @@ Route::middleware(['auth', 'role:desainer,pelanggan'])->group(function () {
     Route::get('/requests', [RequestsController::class, 'index'])->name('requests.index');
 });
 
+Route::middleware(['auth', 'role:desainer'])->group(function () {
+    Route::patch('/requests/{id}', [RequestsController::class, 'updateGambar'])->name('requests.updateGambar');
+});
+
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');

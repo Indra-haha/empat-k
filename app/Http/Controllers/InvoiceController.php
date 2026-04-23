a53403c14307c26e7b5c8b3c9f2222d4f8d0b32b
Indra-haha <124240180@student.upnyk.ac.id>
2026-04-24 01:42:47 +0700

    feat : add function upload bukti tagihan dan cek aautorisasi role

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -34,6 +34,7 @@ class InvoiceController extends Controller
                     'total' => $invoice?->total_amount ?? null,
                     'img_tagihan' => $invoice?->url_img_tagihan ?? null,
                     'status' => $order->latestStatus->status,
+                    'img_bukti' => $invoice?->url_img_bukti ?? null,
                 ];
             });
         }
@@ -46,13 +47,13 @@ class InvoiceController extends Controller
 
     public function store(Request $request)
     {
+        $this->authorizeAction('create', Invoice::class);
         $this->validate($request, [
             'order_id' => 'required|exists:orders,order_id',
             'invoice_no' => 'required|string|unique:invoices,invoice_number',
             'url_img_tagihan' => 'required|file|mimes:jpg,jpeg,png|max:5120',
             'total_amount' => 'required|numeric',
         ]);
-        sleep(2);
         try {
             if ($request->hasFile('url_img_tagihan')) {
                 $file = $request->file('url_img_tagihan');
@@ -89,9 +90,41 @@ class InvoiceController extends Controller
 
         return back()->withErrors(['url_img_tagihan' => 'File tidak ditemukan']);
     }
+    public function uploadBukti(Request $request)
+    {
+        $this->authorizeAction('update', Invoice::class);
+        $this->validate($request, [
+            'invoice_no' => 'required|string',
+            'url_img_bukti' => 'required|file|mimes:jpg,jpeg,png|max:5120',
+        ]);
+        sleep(2);
+        try {
+            if ($request->hasFile('url_img_bukti')) {
+                $file = $request->file('url_img_bukti');
+
+                // Gunakan nama file yang bersih
+                 $fileName = str_replace(['/', '\\', ' '], '-', $request->invoice_no) . '.png';
+
+                // Simpan ke disk 'private'
+                $path = $file->storeAs('bukti', $fileName, 'private');
+
+                Invoice::where('invoice_number', $request->invoice_no)->update([
+                    'url_img_bukti' => $path,
+                ]);
+
+                return back()->with('success', 'Bukti Pembayaran Berhasil Disimpan!');
+            }
+        } catch (\Exception $e) {
+            // Jika gagal simpan/upload, kirim error ke frontend
+            return back()->withErrors(['error' => 'Gagal simpan: ' . $e->getMessage()]);
+        }
+
+        return back()->withErrors(['url_img_bukti' => 'File tidak ditemukan']);
+    }
     public function show(Request $request)
     {
         $this->authorizeAction('view', Invoice::class);
+
         $path = $request->query('file'); // Isinya: tagihan/INV-xxx.png
 
         if (!auth()->check()) {

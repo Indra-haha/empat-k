5e3153f3f1acabd667a0350a5897a90ab19d5be0
Indra-haha <124240180@student.upnyk.ac.id>
2026-04-23 07:51:12 +0700

    feat :nampilin tagihan di pelanggan & skema upload tagihan di accounting

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -3,26 +3,42 @@
 namespace App\Http\Controllers;
 use App\Models\Invoice;
 use App\Models\OrderStatusHistory;
+use App\Models\Order;
 use Inertia\Inertia;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 class InvoiceController extends Controller
 {
     public function index()
     {
         $this->authorizeAction('view', Invoice::class);
+        $role = Auth::user()->role;
+        $invoices = [];
+        if ($role === 'pelanggan') {
+            $invoices = Order::with('invoice', 'latestStatus')
+                ->where('user_id', Auth::user()->user_id)
+                ->whereHas('latestStatus', function ($query) {
+                    $query->whereIn('status', ['partial_paid', 'full_paid']);
+                })
+                ->get();
 
-        $invoices = Invoice::with('latestStatus')->get()->map(function ($invoice) {
-            return [
-                'no' => $invoice->id,
-                'order' => $invoice->order_id,
-                'invoice_no' => $invoice->invoice_number,
-                'total' => $invoice->total_amount,
-                'img' => $invoice->url_img,
-                'status' => $invoice->latestStatus?->status ?? 'No Status',
-            ];
-        });
-        return Inertia::render('accounting/InvoicePage/InvoiceList', [
+            $invoices = $invoices->map(function ($order) {
+                $invoice = $order->invoice
+                    ? $order->invoice->first()
+                    : $order->invoice;
+
+                return [
+                    'invoice_no' => $invoice?->invoice_number ?? null,
+                    'order_id' => $order->order_id,
+                    'total' => $invoice?->total_amount ?? null,
+                    'img_tagihan' => $invoice?->url_img_tagihan ?? null,
+                    'status' => $order->latestStatus->status,
+                ];
+            });
+        }
+
+        return Inertia::render("${role}/InvoicePage/InvoiceList", [
             'nota' => $invoices
         ]);
 
@@ -36,7 +52,7 @@ class InvoiceController extends Controller
             'url_img_tagihan' => 'required|file|mimes:jpg,jpeg,png|max:5120',
             'total_amount' => 'required|numeric',
         ]);
-
+        sleep(2);
         try {
             if ($request->hasFile('url_img_tagihan')) {
                 $file = $request->file('url_img_tagihan');
@@ -57,11 +73,11 @@ class InvoiceController extends Controller
                 );
 
                 OrderStatusHistory::with('order')
-                ->where('order_id', $request->order_id)
-                ->create([
-                    'order_id' => $request->order_id,
-                    'status' => 'partial_paid',
-                    'created_by' => Auth::user()->user_id,
+                    ->where('order_id', $request->order_id)
+                    ->create([
+                        'order_id' => $request->order_id,
+                        'status' => 'partial_paid',
+                        'created_by' => Auth::user()->user_id,
                     ]);
 
                 return back()->with('success', 'Tagihan Berhasil Disimpan!');
@@ -72,5 +88,25 @@ class InvoiceController extends Controller
         }
 
         return back()->withErrors(['url_img_tagihan' => 'File tidak ditemukan']);
+    }
+    public function show(Request $request)
+    {
+        $this->authorizeAction('view', Invoice::class);
+        $path = $request->query('file'); // Isinya: tagihan/INV-xxx.png
+
+        if (!auth()->check()) {
+            abort(401, 'Session habis, silakan login ulang.');
+        }
+
+        if (!$path)
+            abort(400, 'Path file kosong.');
+
+        $fullPath = storage_path("app/private/{$path}");
+
+        if (!file_exists($fullPath)) {
+            abort(404, "File tidak ada di: " . $fullPath);
+        }
+
+        return response()->file($fullPath);
     }
 }

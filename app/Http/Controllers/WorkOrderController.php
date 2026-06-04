67a80d184a484e7f2960b04c2803e8ba2337b6cc
Indra-haha <124240180@student.upnyk.ac.id>
2026-06-04 09:16:39 +0700

    fix : response checking order in controller

--- app/Http/Controllers/WorkOrderController.php
+++ app/Http/Controllers/WorkOrderController.php
@@ -73,7 +73,7 @@ class WorkOrderController extends Controller
                 'status_bukti' => $order->latestInvoiceStatus->status_bukti ?? null,
                 'fee' => $order->request->fee ?? null,
             ];
-            if ($status === 'process') {
+            if ($status === 'process' || $status === 'checking') {
                 return array_merge($common, [
                     'ukuran' => $order->workOrder->ukuran,
                     'bahan' => $order->workOrder->bahan,

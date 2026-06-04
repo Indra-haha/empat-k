989d8581a1fd0c924872074b6fc0e29b8b2e1f4f
Indra-haha <124240180@student.upnyk.ac.id>
2026-06-04 09:00:39 +0700

    fix: response  detail work order data

--- resources/js/Pages/cs/WorkOrderPage/WorkOrderList.tsx
+++ resources/js/Pages/cs/WorkOrderPage/WorkOrderList.tsx
@@ -35,7 +35,7 @@ console.log("Received workOrders prop:", workOrders); // Debug: Lihat data yang 
 
     const handleCardClick = (order: OrdersCSProps | WorkOrderProps) => {
       console.log("Card clicked with order:", order); 
-      if (order.status === "process") {
+      if (order.status === "process" || order.status === "checking") {
         setopenDetailWorkOrder(order as WorkOrderProps); 
         setopenDetailOrder(null);
       } else {

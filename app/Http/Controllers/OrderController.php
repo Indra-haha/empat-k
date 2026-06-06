6d32eba0d1f5636d85bc5a3e5fc75f76e4165017
Indra-haha <124240180@student.upnyk.ac.id>
2026-06-07 03:28:38 +0700

    fix: remove console

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -206,7 +206,6 @@ class OrderController extends Controller
 
     public function updateStatus($id)
     {
-        dd($id);
         $this->authorizeAction('update', Order::class);
         $order = Order::where('order_id', $id)->firstOrFail();
         $role = Auth::user()->role;

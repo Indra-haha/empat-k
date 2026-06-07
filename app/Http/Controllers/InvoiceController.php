48d7839dacd23833b62d14404f0e509b0315fd6b
Indra-haha <124240180@student.upnyk.ac.id>
2026-06-07 18:24:03 +0700

    fix: format public id

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -83,7 +83,7 @@ class InvoiceController extends Controller
             $extension      = $request->file('url_img_tagihan')->getClientOriginalExtension();
             $storedPublicId = $this->cloudinary->upload($request->file('url_img_tagihan')->getRealPath(), [
                 'folder' => 'tagihan',
-                'publicId' => 'INV-' . date('ymd') . '-' . $order->order_id,
+                'public_id' => 'INV-' . date('ymd') . '-' . $order->order_id,
                 'type' => 'private',
             ]);
 
@@ -92,7 +92,7 @@ class InvoiceController extends Controller
                 [
                     'invoice_number'  => $request->invoice_no,
                     'total_amount'    => $request->total_amount,
-                    'url_img_tagihan' => $storedPublicId['publicId'] . "." . $extension,  // contoh: "tagihan/INV-250607-12.jpg"
+                    'url_img_tagihan' => $storedPublicId['public_id'] . "." . $extension,  // contoh: "tagihan/INV-250607-12.jpg"
                 ]
             );
 
@@ -127,12 +127,12 @@ class InvoiceController extends Controller
 
             $storedPublicId = $this->cloudinary->upload($file->getRealPath(), [
                 'folder' => 'bukti',
-                'publicId' => 'INV-' . date('ymd') . '-' . $request->order_id,
+                'public_id' => 'INV-' . date('ymd') . '-' . $request->order_id,
                 'type' => 'private',
             ]);
 
             Invoice::where('invoice_number', $request->invoice_no)->update([
-                'url_img_bukti' => $storedPublicId['publicId'] . '.' . $extension, // contoh: "bukti/INV-250607-12.png"
+                'url_img_bukti' => $storedPublicId['public_id'] . '.' . $extension, // contoh: "bukti/INV-250607-12.png"
                 'status_bukti'  => 'pending',
             ]);
 
